8d821d03515e52ebf703db446272899484b1ec28
Refs <lhx1010339808@outlook.com>
2018-05-06 22:04:03 +0800

    .

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,13 @@ module.exports = {
     },
     module: {
         rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
         ]
     },
     resolve: {
